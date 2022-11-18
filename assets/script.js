b87de9226e81ec39cbe8a151dd9a348983ed7d05
Sai Chandu K <91937496+Chandu-5@users.noreply.github.com>
2022-11-18 18:23:07 +0530

    faqPage
    created FAQ page. Added icons in college.html.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// newPageNavigator
 function goToNewPage() {
     var url = document.getElementById('drop').value;
     if (url != 'none') {
@@ -5,6 +6,7 @@ function goToNewPage() {
     }
 }
 
+// nav animation
 $(window).on("scroll", function() {
     if($(window).scrollTop() > 100) {
         $(".header").addClass("active");
@@ -13,6 +15,23 @@ $(window).on("scroll", function() {
        $(".header").removeClass("active");
     }
 });
+
+// collapse
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
 
 $(document).ready(function () {
 
